47bbd52a71c921fabca0f7dc584d36eb0ac5f83a
ashi14 <43583044+ashi14@users.noreply.github.com>
2018-09-28 21:55:50 +0530

    Add files via upload

---
+++ assign2.py.py
@@ -0,0 +1,36 @@
+#print anything you want on screen
+print("hello world1 we are learning python")
+
+#join two strings using '+'
+r="mile" + "stone"
+print(r)
+
+#take the input of 3 variables x,y,z. print their values on screen
+x= input("enter the value of x:")
+y= input("enter the value of y:")
+z= input("enter the value of z:")
+print(x,y,z)
+
+#print "let's get started" on screen
+  print('let\'s get started')
+print ("\"let's get started\" ")
+
+#print the following values using placeholders
+#s= "acadview"
+#course="python"
+#fees=5000
+
+s="acadview"
+course="python"
+fees=5000
+print(" %s %s %d %(s,course,fees))
+
+#find the area of circle
+      #pi=3.14
+      #take radius as input from user.
+      #print area of circle
+pi= 3.14
+r.float(input("enter thre radius"))
+      print(pi*r*r)
+      print(pi*r**2)
+      
